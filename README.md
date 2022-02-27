68a1f542d91dabd770acfd9e6530b37051d29875
Dave Faliskie <davefaliskie@gmail.com>
2022-02-27 14:56:16 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Flutter Shorts
+
+Example code related to simpler flutter topics covered in my YouTube shorts.
+
+[View the shorts here](https://youtube.com/playlist?list=PL_D-RntzgLvbiFHkT06q2SS-t4yrTYqAu)
---
+++ lib/main.dart
@@ -0,0 +1,97 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:logger/logger.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: '1ManStartup.com'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String _message = "Click Subscribe";
+  Logger logger = Logger();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        elevation: 0,
+      ),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.red, Colors.yellow],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            )
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Spacer(),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(_message),
+              ),
+              TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.red,
+                  primary: Colors.white,
+                ),
+                child: Text("Subscribe", style: TextStyle(color: Colors.white)),
+                onPressed: () {
+                  setState(() {
+                    _message = "Subscribed!";
+                  });
+                  HapticFeedback.heavyImpact();
+                  logger.d("Subscribed");
+                },
+              ),
+              Spacer()
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+
+  Widget repeatText({text, count}) {
+    List<Widget> children = [];
+    for(int i = 1; i <= count; i++) {
+      children.add(Text(text));
+    }
+    return Column(
+      children: children,
+    );
+  }
+}
